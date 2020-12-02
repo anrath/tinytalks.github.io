b0104f851d7a747afc27c680b99c2c5a6834ec5c
anrath <aklounts@gmail.com>
2020-12-01 21:30:15 -0800

    adding images to portfolio with modal gallery

--- js/creative.js
+++ js/creative.js
@@ -4,45 +4,54 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-(function($) {
+(function ($) {
     "use strict"; // Start of use strict
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
+    $("a.page-scroll").bind("click", function (event) {
         var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
+        $("html, body")
+            .stop()
+            .animate(
+                {
+                    scrollTop: $($anchor.attr("href")).offset().top - 50,
+                },
+                1250,
+                "easeInOutExpo"
+            );
         event.preventDefault();
     });
 
     // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    })
+    $("body").scrollspy({
+        target: ".navbar-fixed-top",
+        offset: 51,
+    });
 
     // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
-        $('.navbar-toggle:visible').click();
+    $(".navbar-collapse ul li a").click(function () {
+        $(".navbar-toggle:visible").click();
     });
 
     // Fit Text Plugin for Main Header
-    $("h1").fitText(
-        1.2, {
-            minFontSize: '35px',
-            maxFontSize: '65px'
-        }
-    );
+    $("h1").fitText(1.2, {
+        minFontSize: "35px",
+        maxFontSize: "65px",
+    });
 
     // Offset for Main Navigation
-    $('#mainNav').affix({
+    $("#mainNav").affix({
         offset: {
-            top: 100
-        }
-    })
+            top: 100,
+        },
+    });
 
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
-
 })(jQuery); // End of use strict
+$(function () {
+    $(".pop").on("click", function () {
+        $(".imagepreview").attr("src", $(this).find("img").attr("src"));
+        $("#imagemodal").modal("show");
+    });
+});
